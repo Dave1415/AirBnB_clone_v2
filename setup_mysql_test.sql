b5a7a87cfd2a4e12096ca5e5d3d24badb24d5af3
dave1415 <dawitgetaneh99@gmail.com>
2022-12-09 14:40:08 +0000

    dave

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,15 +1,7 @@
--- prepares a MySQL server for the project - test:
-
+-- Creates a database called hbnb_test_db in the current MySQL server
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER
-       IF NOT EXISTS 'hbnb_test'@'localhost'
-       IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES
-      ON `hbnb_test_db`.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
-GRANT SELECT
-      ON `performance_schema`.*
-      TO 'hbnb_test'@'localhost'
-      IDENTIFIED BY 'hbnb_test_pwd';
-FLUSH PRIVILEGES;
+-- creates the MySQL server user hbnb_test
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- Grants Permissions for user hbnb_test
+GRANT ALL ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
