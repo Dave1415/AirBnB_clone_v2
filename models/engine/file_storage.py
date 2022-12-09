753b36a90d502b207728a4eed2d1262865ad1cef
dave1415 <dawitgetaneh99@gmail.com>
2022-12-09 15:08:00 +0000

    dave

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,6 +25,12 @@ class FileStorage:
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
+    def delete(self, obj=none):
+        """
+             delete object from object if it is inside and if the object is not available the code do nothing
+        """
+        if (obj):
+            self.__objects.pop("{}.{}".format(type(obj).__name__, obj.id))
     def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
